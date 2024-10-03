6714a564fe62a571b6a16f328ee65c896fcd5d41
Ajith Chakkadath <130172872+Ajith-Chakkadath@users.noreply.github.com>
2024-10-03 10:32:43 +0530

    Section2 completed
    Not responsive

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Herosection from './components/Herosection';
 import LocationSection from './components/LocationSection';
+import SizeSection from './components/SizeSection';
 
 function App() {
   return (
     <div className="App">
    <Herosection />
    <LocationSection />
+   <SizeSection />
     </div>
   );
 }
---
+++ src/components/SizeSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SizeSection = () => {
+  return (
+    <div>SizeSection</div>
+  )
+}
+
+export default SizeSection
